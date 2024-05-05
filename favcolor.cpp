4026d336a1a9150d24e96c8a347c004023106e89
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-05-05 22:45:10 +0530

    third commit

--- favcolor.cpp
+++ favcolor.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 int main()
 {
-    string colour;
+     string colour;
     cout<<"what's your favourite colour?\n";
     getline(cin,colour);
     cout<<"Hey "<< colour<<" is my favourite colour too."<<endl;
     return 0;
+
+
+
+    // string first_name , last_name;
+    // cout<< " enter your first name: "<<endl;
+    // getline(cin,first_name);
+    // cout<<("Enter your last name: ");
+    // getline(cin,last_name);
+
+    // cout<<first_name <<" "<< last_name<<endl;
+
 }
