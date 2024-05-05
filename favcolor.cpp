9221b25e514900902c9a182bb819f42d1408e7eb
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-05-05 22:25:59 +0530

    full stop added

--- favcolor.cpp
+++ favcolor.cpp
@@ -7,6 +7,6 @@ int main()
     char colour[20];
     cout<<"what's your favourite colour?\n";
     cin>>colour;
-    cout<<colour<<" is my favourite colour too";
+    cout<<colour<<" is my favourite colour too.";
     return 0;
 }
