8304c8fdbe8384003fee77b680864293b106a640
yadav-surendra <ysurendraaprasad2007@gmail.com>
2024-05-11 20:12:26 +0530

    commit

---
+++ macros.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+#define ENDMSG cout<< "program ends here.\n"    //defining macro. they act as shortcut of the actual code whatever is written after the macro name.
+#define MYINT int16_t
+
+int main(){
+
+MYINT a = 4;
+cout<< a << endl;
+
+ENDMSG;
+}
